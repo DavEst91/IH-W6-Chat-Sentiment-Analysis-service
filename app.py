82869dcc163d5462957818ed55a3d645cead5ec8
DavEst91 <davidestebanmendoza@gmail.com>
2020-05-03 21:45:39 +0200

    corrected

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from src.config import PORT,IP
-from src.app import app
+from src.app import app as application
 import src.controllers.users
 import src.controllers.chats
 import src.controllers.message
 import src.controllers.reccomender_system
 
-app.run(debug=True)
+application.run(debug=True)
