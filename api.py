bfe265bbcf43a8733f65cd5cf0b6390f6c9af819
DavEst91 <davidestebanmendoza@gmail.com>
2020-05-04 00:11:40 +0200

    try

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 from src.config import PORT,IP
-from src.app import app as application
+from src.app import app as app
 import src.controllers.users
 import src.controllers.chats
 import src.controllers.message
 import src.controllers.reccomender_system
+import os
+port = int(os.environ.get("PORT", 5000))
 
-application.run(debug=True)
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0",port=PORT)
